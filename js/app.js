e9dbce7125d6c27460be5595c2746a2c700af23d
Meheryar <meheryartata@gmail.com>
2018-06-17 18:41:10 +0530

    refactor filter function into 2 subfunctions for easier understanding

--- js/app.js
+++ js/app.js
@@ -1,44 +1,52 @@
-let myViewModel = {
-    searchContent: ko.observable(''),
+var myViewModel = {
     locations: ko.observableArray(location_list),
-    markers_list: ko.observableArray(initMap())
+    markersList: ko.observableArray(initMap()),
+    searchContent: ko.observable('')
 };
-
-
-// Activate knockout.js
-ko.applyBindings(myViewModel);
 
 
 //If search string is in list, create new filtered list
 
 function filter() {
 
-    let filtered_locations = [];
     let search = myViewModel.searchContent().toString();
 
+    locationFilter(search)
+    markerFilter(search);
+}
 
-    for (i = 0; i < location_list.length; i++) {
+
+function locationFilter(search) {
+
+    let filtered_locations = [];
+        for (i = 0; i < location_list.length; i++) {
+
         let location = location_list[i];
 
         if (location.name.toLowerCase().includes(search.toLowerCase())) {
 
             let new_location = new Location(location.name, location.lat, location.lng);
             filtered_locations.push(new_location);
+            myViewModel.locations(filtered_locations);
 
-        }
-    }
-
-    myViewModel.locations(filtered_locations);
-
-    for (i = 0; i < myViewModel.markers_list().length; i++) {
-
-        if (myViewModel.markers_list()[i].title.toLowerCase().indexOf(search.toLowerCase()) == -1) {
-            myViewModel.markers_list()[i].setVisible(false);
-        } else {
-            myViewModel.markers_list()[i].setVisible(true);
         }
     }
 }
+
+
+
+function markerFilter(search) {
+
+    for (i = 0; i < myViewModel.markersList().length; i++) {
+
+        if (myViewModel.markersList()[i].title.toLowerCase().indexOf(search.toLowerCase()) == -1) {
+            myViewModel.markersList()[i].setVisible(false);
+        } else {
+            myViewModel.markersList()[i].setVisible(true);
+        }
+    }
+}
+
 
 
 /*
@@ -50,12 +58,14 @@ function clickMarker(data, event) {
 
     let clicked_location = [];
     let i = 0;
-    let location = myViewModel.markers_list()[i].title;
+    let location = myViewModel.markersList()[i].title;
     while (location != event.target.text) {
         i++;
-        location = myViewModel.markers_list()[i].title;
+        location = myViewModel.markersList()[i].title;
     }
-    google.maps.event.trigger(myViewModel.markers_list()[i], 'click');
+    google.maps.event.trigger(myViewModel.markersList()[i], 'click');
 }
 
 
+// Activate knockout.js
+ko.applyBindings(myViewModel);
